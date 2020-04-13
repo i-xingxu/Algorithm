5b8e165ced7738f71f4bf7b3f77f425dd23f9564
xingxu <im1991@msn.com>
2020-04-13 23:01:43 +0800

    二分查找法

--- xuanze.py
+++ xuanze.py
@@ -1,20 +1,20 @@
-#coding=utf-8
+# coding=utf-8
 
 import numpy as np
 
-def xuanze_sort(lst):
 
-    if len(lst)<=0:
+def xuanze_sort(lst):
+    if len(lst) <= 0:
         return -1
-    for i in range(0,len(lst)-1):
-        for j in range(i+1,len(lst)):
-            if lst[i]>lst[j]:
-                lst[i],lst[j]=lst[j],lst[i]
+    for i in range(0, len(lst) - 1):
+        for j in range(i + 1, len(lst)):
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
 
     return lst
 
 
-nums=np.random.randint(200,size=10)
+nums = np.random.randint(200, size=10)
 print(nums)
 print(xuanze_sort(nums))
 # 0   1       173 191 42 51
